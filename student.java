8a8cf9cae170d9b729167a568d5de972c1c7f871
Moksh <moxhsharma878@gmail.com>
2025-03-25 18:54:30 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,21 @@
+public class student {
+    private String studentid;
+    private String studentname;
+    private String Studentgrade;
+    //defalt construictor 
+    public student (){
+        this.studentid = "rajesh12";
+        this.studentname = "rajesh";
+        this.Studentgrade = "0";
+    }
+        //parameter constructor
+        public student(String studentid, String studentname, String Studentgrade){
+            this.studentid = studentid;
+            this.studentname = studentname;
+            this.Studentgrade = Studentgrade;
+        }
+    
+    public static void main(String[] args) {
+        
+    }
+}
